096db8d9b19d2899a656d9104e61408e59ce0333
Winson8222 <winson8222@hotmail.com>
2023-07-06 01:15:27 +0800

    script changed

--- JSONGenwithpostres/shutdown.ps1
+++ JSONGenwithpostres/shutdown.ps1
@@ -1,21 +1,24 @@
 param (
-    [Parameter(Mandatory = $true)]
-    [int]$PortNumber
+    [Parameter(Mandatory = $true, ValueFromRemainingArguments = $true)]
+    [int[]]$PortNumbers
 )
 
-$netTCPConnections = Get-NetTCPConnection -LocalPort $PortNumber -ErrorAction SilentlyContinue
+foreach ($PortNumber in $PortNumbers) {
+    $netTCPConnections = Get-NetTCPConnection -LocalPort $PortNumber -ErrorAction SilentlyContinue
 
-if ($netTCPConnections) {
-    $process = Get-Process -Id $netTCPConnections.OwningProcess | Select-Object -First 1
+    if ($netTCPConnections) {
+        $process = Get-Process -Id $netTCPConnections.OwningProcess | Select-Object -First 1
 
-    if ($process) {
-        Write-Host "Terminating process with ID $($process.Id) and name $($process.Name)"
-        Stop-Process -Id $process.Id -Force
+        if ($process) {
+            Write-Host "Terminating process with ID $($process.Id) and name $($process.Name)"
+            Stop-Process -Id $process.Id -Force
+        }
+        else {
+            Write-Host "No process found running on port $PortNumber."
+        }
     }
     else {
-        Write-Host "No process found running on port $PortNumber."
+        Write-Host "No TCP connections found on port $PortNumber."
     }
 }
-else {
-    Write-Host "No TCP connections found on port $PortNumber."
-}
+
